6d94722780afcf26a8f020bdb93da5f74aee8574
George Kopti <koptig@sheridanollege.ca>
2019-12-13 17:15:28 -0500

    Mohit part did the HTML and consumer support

---
+++ Parking App/View/CustomerSupportVC.swift
@@ -0,0 +1,89 @@
+//
+//  CustomerSupportVC.swift
+//  Parking App
+//
+//  Created by Xcode User on 2019-12-13.
+//  Copyright © 2019 the flying pigs. All rights reserved.
+//
+
+import UIKit
+
+import MessageUI
+
+class CustomerSupportVC: UIViewController, MFMailComposeViewControllerDelegate {
+
+    @IBOutlet var callButton: UIButton!
+    
+    @IBOutlet var emailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func doingCall(){
+         print("Trying to call...")
+         
+         var phoneNumber : String = "3065803931"
+         
+         // URL format tel://1234567890
+         let callString : String = "tel://\(phoneNumber)"
+         
+         //convert string into URL
+         let url = URL(string: callString)
+         
+         print("URL : \(url)")
+         
+         //check if any application is available to execute the created URL
+         if UIApplication.shared.canOpenURL(url!){
+             
+             //check the version of iOS on the device on which the app is running
+             if #available(iOS 10, *){
+                 //for all the version of iOS 10 onwards
+                 //if app is available open the URL
+                 UIApplication.shared.open(url!)
+             }else{
+                 //for iOS version 9 and older
+                 UIApplication.shared.openURL(url!)
+             }
+         }else{
+             print("Can't place call")
+         }
+     }
+    
+    @IBAction func mailButtonPressed(_ sender: Any) {
+        
+
+      
+        if MFMailComposeViewController.canSendMail() {
+         
+             let mailComposeViewController = MFMailComposeViewController()
+             mailComposeViewController.mailComposeDelegate = self
+             mailComposeViewController.setToRecipients(["abc@abc.abc"])
+             mailComposeViewController.setSubject("Subject")
+             mailComposeViewController.setMessageBody("Hello!!!", isHTML: false)
+         
+             present(mailComposeViewController, animated: true, completion: nil)
+         
+        }
+    }
+
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+     
+         controller.dismiss(animated: true, completion: nil)
+     
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parking App/View/ParkingManualVC.swift
@@ -0,0 +1,50 @@
+//
+//  ParkingManualVC.swift
+//  Parking App
+//
+//  Created by Xcode User on 2019-12-13.
+//  Copyright © 2019 the flying pigs. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ParkingManualVC: UIViewController, WKUIDelegate{
+
+    @IBOutlet  var webView: WKWebView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let htmlPath = Bundle.main.path(forResource: "parkingManual", ofType: "html")
+        // Do any additional setup after loading the view.
+        let url = URL(fileURLWithPath: htmlPath!)
+        let request = URLRequest(url: url)
+        
+        self.webView.load(request)
+
+        
+    }
+    
+    
+    override func loadView() {
+        //customize or configure your web view appearance
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
